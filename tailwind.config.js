115e3b52e104c0d82f285c01ee79ec7a1cc3a646
LucasCavalheri <cav.catecumenal@gmail.com>
2023-05-15 17:16:14 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['var(--font-inter)', 'sans-serif'],
+    },
+    extend: {
+      backgroundImage: {
+        'hero-image': "url('/images/hero-bg.png')",
+      },
+      fontFamily: {
+        mono: ['var(--font-plex-mono)', 'monospace'],
+      },
+      boxShadow: {
+        button: '0px 0px 68px 7px rgba(5, 150, 105, 0.4)',
+      },
+    },
+  },
+  plugins: [],
+}
