0fe6b37053342211ef390e85e1d5da1537825f96
LucasCavalheri <cav.catecumenal@gmail.com>
2023-05-16 09:52:26 -0300

    feat: Hygraph: popula as KnownTechs

--- app/components/pages/home/known-techs/index.tsx
+++ app/components/pages/home/known-techs/index.tsx
@@ -1,20 +1,20 @@
-import { SectionsTitle } from '@/app/components/sections-title';
-import { KnownTech } from './known-tech';
-import { TbBrandNextjs } from 'react-icons/tb';
+import { SectionsTitle } from '@/app/components/sections-title'
+import { KnownTech } from './known-tech'
+import { KnownTech as IKnownTech } from '@/app/types/projects'
 
-export const KnownTechs = () => {
+type KnownTechsProps = {
+  techs: IKnownTech[]
+}
+
+export const KnownTechs = ({ techs }: KnownTechsProps) => {
   return (
     <section className='container py-16'>
       <SectionsTitle title='competências' subtitle='Conhecimentos' />
       <div className='w-full grid grid-cols-[repeat(auto-fit,minmax(264px,1fr))] gap-3 mt-[60px]'>
-        {Array.from({ length: 5 }).map((_, index) => (
-          <KnownTech key={index} tech={{
-            icon: <TbBrandNextjs />,
-            name: 'Next.js',
-            startDate: '2022-01-01'
-          }} />
+        {techs?.map((tech) => (
+          <KnownTech key={tech.name} tech={tech} />
         ))}
       </div>
     </section>
-  );
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ export default async function Home() {
   return (
     <>
       <HeroSection homeInfo={pageData} />
-      <KnownTechs />
+      <KnownTechs techs={pageData.knownTechs} />
       <HighlightedProjects />
       <WorksExperience />
     </>
