0ac4040771f8b85708f45c58ec6ca52552c02e2d
LucasCavalheri <cav.catecumenal@gmail.com>
2023-05-15 21:18:33 -0300

    fix: adiciona o texto no lugar correto

--- app/components/pages/home/known-techs/known-tech/index.tsx
+++ app/components/pages/home/known-techs/known-tech/index.tsx
@@ -15,10 +15,10 @@ export const KnownTech = ({ tech }: KnownTechProps) => {
   return (
     <div className='p-6 rounded-lg bg-gray-600/20 text-gray-500 flex flex-col gap-2 hover:text-emerald-500 hover:bg-gray-600/30 transition-all'>
       <div className='flex items-center justify-between'>
-        <p className='font-medium'>{tech.name} de experiência</p>
+        <p className='font-medium'>{tech.name}</p>
         {tech.icon}
       </div>
-      <span>{relativeTime}</span>
+      <span>{relativeTime}  de experiência</span>
     </div>
   );
 }
