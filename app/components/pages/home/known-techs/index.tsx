2c1186ba1a1bf5c0da26c3efdfd3452f4da3d223
LucasCavalheri <cav.catecumenal@gmail.com>
2023-05-15 22:02:54 -0300

    fix: adiciona parenteses faltando no grid

--- app/components/pages/home/known-techs/index.tsx
+++ app/components/pages/home/known-techs/index.tsx
@@ -6,7 +6,7 @@ export const KnownTechs = () => {
   return (
     <section className='container py-16'>
       <SectionsTitle title='competências' subtitle='Conhecimentos' />
-      <div className='w-full grid grid-cols-[repeat(auto-fit,minmax(264px,1fr)] gap-3 mt-[60px]'>
+      <div className='w-full grid grid-cols-[repeat(auto-fit,minmax(264px,1fr))] gap-3 mt-[60px]'>
         {Array.from({ length: 5 }).map((_, index) => (
           <KnownTech key={index} tech={{
             icon: <TbBrandNextjs />,
