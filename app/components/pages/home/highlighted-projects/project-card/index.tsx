1d2e67f18e9944eae5e53ede53b4b66963fddd0e
LucasCavalheri <cav.catecumenal@gmail.com>
2023-05-17 11:57:32 -0300

    feat: melhora a acessilidade para o usuário ir para a página de um projeto

--- app/components/pages/home/highlighted-projects/project-card/index.tsx
+++ app/components/pages/home/highlighted-projects/project-card/index.tsx
@@ -3,6 +3,7 @@ import { Link } from '@/app/components/link'
 import { TechBadge } from '@/app/components/tech-badge'
 import { Project } from '@/app/types/projects'
 import { HiArrowNarrowRight } from 'react-icons/hi'
+import { Button } from '@/app/components/button'
 
 type ProjectCardProps = {
   project: Project
@@ -12,23 +13,27 @@ export const ProjectCard = ({ project }: ProjectCardProps) => {
   return (
     <div className='flex gap-6 lg:gap-12 flex-col lg:flex-row'>
       <div className='w-full h-full'>
-        <Image
-          src={project.thumbnail.url}
-          alt={`Thumbnail do Projeto ${project.title}`}
-          width={420}
-          height={304}
-          className='w-full h-[200px] sm:h-[300px] lg:w-[420px] lg:min-h-full object-cover rounded-lg'
-        />
+        <Link href={`/projects/${project.slug}`}>
+          <Image
+            src={project.thumbnail.url}
+            alt={`Thumbnail do Projeto ${project.title}`}
+            width={420}
+            height={304}
+            className='w-full h-[200px] sm:h-[300px] lg:w-[420px] lg:min-h-full object-cover rounded-lg'
+          />
+        </Link>
       </div>
       <div>
         <h3 className='flex items-center gap-3 font-medium text-lg text-gray-50'>
-          <Image
-            src='/images/icons/project-title-icon.svg'
-            alt='Ícone para enfeitar o título do projeto'
-            width={20}
-            height={20}
-          />
-          {project.title}
+          <Link href={`/projects/${project.slug}`}>
+            <Image
+              src='/images/icons/project-title-icon.svg'
+              alt='Ícone para enfeitar o título do projeto'
+              width={20}
+              height={20}
+            />
+            {project.title}
+          </Link>
         </h3>
         <p className='text-gray-400 my-6'>{project.shortDescription}</p>
         <div className='flex gap-x-2 gap-y-3 flex-wrap mb-8 lg:max-w-[350px]'>
@@ -40,8 +45,9 @@ export const ProjectCard = ({ project }: ProjectCardProps) => {
           ))}
         </div>
         <Link href={`/projects/${project.slug}`}>
-          Ver Projeto
-          <HiArrowNarrowRight />
+          <Button className='w-max shadow-button'>
+            Ver Projeto <HiArrowNarrowRight />
+          </Button>
         </Link>
       </div>
     </div>
