8709ffdcd47b95af393c517f3a8a56e7722d4421
LucasCavalheri <cav.catecumenal@gmail.com>
2023-05-17 18:25:12 -0300

    refactor: pequena mudança de letra

--- app/components/pages/home/known-techs/index.tsx
+++ app/components/pages/home/known-techs/index.tsx
@@ -9,7 +9,7 @@ type KnownTechsProps = {
 export const KnownTechs = ({ techs }: KnownTechsProps) => {
   return (
     <section className='container py-16'>
-      <SectionsTitle title='competências' subtitle='Conhecimentos' />
+      <SectionsTitle title='Competências' subtitle='conhecimentos' />
       <div className='w-full grid grid-cols-[repeat(auto-fit,minmax(264px,1fr))] gap-3 mt-[60px]'>
         {techs?.map((tech) => (
           <KnownTech key={tech.name} tech={tech} />
