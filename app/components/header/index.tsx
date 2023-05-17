fb0b1886c94a2dc5acf5c90bd49abac789e94f43
LucasCavalheri <cav.catecumenal@gmail.com>
2023-05-17 15:03:57 -0300

    adiciona a logo

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-// import Image from 'next/image'
+import Image from 'next/image'
 import Link from 'next/link'
 import { NavItem } from './nav-item'
 
@@ -20,8 +20,7 @@ export const Header = () => {
     <header className='absolute top-0 w-full z-10 h-24 flex items-center justify-center'>
       <div className='container flex items-center justify-between'>
         <Link href='/'>
-          {/* <Image width={58} height={49} src='' alt='Logo Lucas Cavalheri' /> */}
-          Logo Image
+          <Image width={100} height={100} src='/images/LC.png' alt='Logo Lucas Cavalheri' />
         </Link>
         <nav className='flex items-center gap-4 sm:gap-10'>
           {NAV_ITEMS.map((item) => (
