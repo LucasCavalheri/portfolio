75b697bdad8bc648bbb76db70ecdca286be9508f
LucasCavalheri <cav.catecumenal@gmail.com>
2023-05-26 19:12:34 -0300

    optimization: SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,27 +10,34 @@ import './globals.css'
 export const metadata = {
   title: {
     default: 'LC Dev',
-    template: '%s | LC Dev'
+    template: '%s | LC Dev',
   },
+  authors: [
+    { name: 'Lucas Cavalheri', url: 'https://linkedin.com/in/lucas-cavalheri' },
+  ],
+  creator: 'Lucas Cavalheri',
   icons: '/favicon.ico',
   openGraph: {
     images: [
       {
         url: '/images/HomeLC.png',
         width: 1200,
-        height: 630
-      }
+        height: 630,
+      },
     ],
     title: 'LC Dev',
-    description: 'Lucas Cavalheri DEV, desenvolvimento de sites e aplicativos web.',
+    description:
+      'Lucas Cavalheri DEV, desenvolvimento de sites e aplicativos web.',
     type: 'website',
     locale: 'pt_BR',
     url: 'https://lucascavalheri.com.br',
   },
   applicationName: 'LC Dev',
-  description: 'Lucas Cavalheri DEV, desenvolvimento de sites e aplicativos web.',
+  description:
+    'Lucas Cavalheri DEV, desenvolvimento de sites e aplicativos web.',
   category: 'Desenvolvimento Web',
-  keywords: 'Desenvolvimento Web, Desenvolvimento de Sites, Desenvolvimento de Aplicativos, Desenvolvimento de Aplicativos Web, Desenvolvimento de Sites Web, Desenvolvimento de Sites em São Paulo, Desenvolvimento de Aplicativos em São Paulo, Desenvolvimento de Aplicativos Web em São Paulo, Desenvolvimento de Sites Web em São Paulo, Desenvolvimento de Sites em SP, Desenvolvimento de Aplicativos em SP, Desenvolvimento de Aplicativos Web em SP, Desenvolvimento de Sites Web em SP, Desenvolvimento de Sites em São Paulo SP, Desenvolvimento de Aplicativos em São Paulo SP, Desenvolvimento de Aplicativos Web em São Paulo SP, Desenvolvimento de Sites Web em São Paulo SP, Desenvolvimento de Sites em SP, Desenvolvimento de Aplicativos em SP, Desenvolvimento de Aplicativos Web em SP, Desenvolvimento de Sites Web em SP, Desenvolvimento de Sites em São Paulo SP, Desenvolvimento de Aplicativos em São Paulo SP, Desenvolvimento de Aplicativos Web em São Paulo SP, Desenvolvimento de Sites Web em São Paulo SP',
+  keywords:
+    'Desenvolvimento Web, Desenvolvimento de Sites, Desenvolvimento de Aplicativos, Desenvolvimento de Aplicativos Web, Desenvolvimento de Sites Web, Desenvolvimento de Sites em São Paulo, Desenvolvimento de Aplicativos em São Paulo, Desenvolvimento de Aplicativos Web em São Paulo, Desenvolvimento de Sites Web em São Paulo, Desenvolvimento de Sites em SP, Desenvolvimento de Aplicativos em SP, Desenvolvimento de Aplicativos Web em SP, Desenvolvimento de Sites Web em SP, Desenvolvimento de Sites em São Paulo SP, Desenvolvimento de Aplicativos em São Paulo SP, Desenvolvimento de Aplicativos Web em São Paulo SP, Desenvolvimento de Sites Web em São Paulo SP, Desenvolvimento de Sites em SP, Desenvolvimento de Aplicativos em SP, Desenvolvimento de Aplicativos Web em SP, Desenvolvimento de Sites Web em SP, Desenvolvimento de Sites em São Paulo SP, Desenvolvimento de Aplicativos em São Paulo SP, Desenvolvimento de Aplicativos Web em São Paulo SP, Desenvolvimento de Sites Web em São Paulo SP',
   classification: 'Desenvolvimento Web',
   robots: 'index, follow',
   publisher: 'LC Dev',
@@ -38,25 +45,25 @@ export const metadata = {
   alternates: {
     canonical: 'https://lucascavalheri.com.br',
     languages: {
-      "pt-BR": 'https://lucascavalheri.com.br',
-    }
+      'pt-BR': 'https://lucascavalheri.com.br',
+    },
   },
 }
 
 const inter = Inter({
   variable: '--font-inter',
-  subsets: ['latin']
+  subsets: ['latin'],
 })
 
 const plexMono = IBM_Plex_Mono({
   variable: '--font-plex-mono',
   subsets: ['latin'],
-  weight: ['400', '500']
+  weight: ['400', '500'],
 })
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang='pt-BR' className={`${inter.variable} ${plexMono.variable}`}>
+    <html lang="pt-BR" className={`${inter.variable} ${plexMono.variable}`}>
       <body>
         <Toaster />
         <BackToTop />
